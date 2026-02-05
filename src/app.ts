03f26ef6ce629768002a157871a1e6aef0e022f5
dockerscarro <dockerscarro@gmail.com>
2026-02-05 11:56:40 +0530

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import routes from "./routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public"))); // for HTML pages
+app.use("/api", routes);
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
