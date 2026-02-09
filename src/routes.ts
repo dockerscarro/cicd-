f1d7f9429442dc93c0c83967f5b9d6e62c38cfb3
dockerscarro <dockerscarro@gmail.com>
2026-02-09 19:27:15 +0530

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signup, checkEmail, saveSpecs } from "./controller";
+import { signup, checkEmail, saveSpecs } from "./controller.ts";
 
 const router = Router();
 
